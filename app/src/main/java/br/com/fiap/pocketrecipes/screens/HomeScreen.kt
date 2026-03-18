8dce85d0c11be58801186bfe2a42274603d39008
Vanessa Amancio <vanessasousa.pro@gmail.com>
2026-03-18 10:35:51 -0300

    feat: add custom top app bar component to home screen

--- app/src/main/java/br/com/fiap/pocketrecipes/screens/HomeScreen.kt
+++ app/src/main/java/br/com/fiap/pocketrecipes/screens/HomeScreen.kt
@@ -1,13 +1,33 @@
 package br.com.fiap.pocketrecipes.screens
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import br.com.fiap.pocketrecipes.R
 import br.com.fiap.pocketrecipes.ui.theme.PocketRecipesTheme
 
 @Composable
@@ -17,7 +37,7 @@ fun HomeScreen(modifier: Modifier = Modifier) {
             .fillMaxSize()
     ) {
         Scaffold(
-            topBar = {},
+            topBar = {MyTopAppBar()},
             bottomBar = {},
             floatingActionButton = {},
         ){ paddingValues ->
@@ -36,5 +56,63 @@ fun HomeScreen(modifier: Modifier = Modifier) {
 private fun HomeScreenPreview() {
     PocketRecipesTheme() {
         HomeScreen()
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyTopAppBar(modifier: Modifier = Modifier) {
+    TopAppBar(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(60.dp),
+        title = {
+            Row(
+                modifier = Modifier
+                    .padding(4.dp)
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Column() {
+                    Text(
+                        text = "Hello, Vanessa!",
+                        style = MaterialTheme.typography.bodyLarge,
+                        color = MaterialTheme.colorScheme.primary,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Text(
+                        text = "vanessa@email.com",
+                        style = MaterialTheme.typography.displaySmall
+                    )
+                }
+                Card(
+                    modifier = Modifier
+                        .size(48.dp),
+                    shape = CircleShape,
+                    colors = CardDefaults
+                        .cardColors(
+                            containerColor = Color.Transparent
+                        ),
+                    border = BorderStroke(
+                        width = 1.dp,
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.user),
+                        contentDescription = "User Image"
+                    )
+                }
+            }
+        }
+    )
+}
+
+@Preview
+@Composable
+private fun MyTopAppBarPreview() {
+    PocketRecipesTheme() {
+        MyTopAppBar()
     }
 }
